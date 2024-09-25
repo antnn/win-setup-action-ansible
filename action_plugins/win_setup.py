b1937f81993e8571ae8dd8fbbc05ca3dba38b2cf
Anton Valishin <anton@valishin.ru>
2024-09-26 00:17:27 +0300

    added iamge selection and static ip configuration logic

--- action_plugins/win_setup.py
+++ action_plugins/win_setup.py
@@ -15,14 +15,74 @@ from ansible.errors import (
 
 default_entry_point = "start.ps1"
 default_main_code_file = "main.cs"
-default_first_logon_cmd = (
-    "start powershell.exe -NoExit -ExecutionPolicy Bypass -File"
-)
+default_first_logon_cmd = "start powershell.exe -NoExit -ExecutionPolicy Bypass -File"
 default_first_logon_cmd = (
     "cmd.exe /C for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do @(if exist %%D:\\%s ( %s %%D:\\%s & goto :break) else (echo Not found)) & :break"
     % (default_entry_point, default_first_logon_cmd, default_entry_point)
 )
 default_install_json_path = "install.json"
+
+
+def image_name_xml_code(name):
+    return f"""<InstallFrom>
+            <MetaData wcm:action="add">
+                <Key>/IMAGE/NAME</Key>
+                <Value>{name}</Value>
+            </MetaData>
+        </InstallFrom>"""
+
+
+def static_ip_xml_code(task, task_vars):
+    static_ip_params = {
+        "interface_identifier": "network_interface",
+        "ip_address": "static_ip_address_cidr",
+        "routes_prefix": "static_route_cidr",
+        "next_hop_address": "static_gateway_ip",
+        "dns_server_address": "static_dns_server",
+    }
+
+    # Populate task_vars with values from task args
+    for param, arg_name in static_ip_params.items():
+        task_vars[param] = task._task.args.get(arg_name, None)
+
+    # Check if any parameter is provided and if all are present
+    if any(task_vars[param] is not None for param in static_ip_params):
+        missing_params = [param for param, value in task_vars.items() if value is None]
+        if missing_params:
+            raise AnsibleAction(
+                message="When configuring static IP, all related parameters must be provided. "
+                f"Missing parameters: {', '.join(missing_params)}"
+            )
+    return f"""<component name="Microsoft-Windows-TCPIP" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                <Interfaces>
+                    <Interface wcm:action="add">
+                        <Ipv4Settings>
+                            <DhcpEnabled>false</DhcpEnabled>
+                        </Ipv4Settings>
+                        <Identifier>{task_vars['interface_identifier']}</Identifier>
+                        <UnicastIpAddresses>
+                            <IpAddress wcm:action="add" wcm:keyValue="1">{task_vars['ip_address']}</IpAddress>
+                        </UnicastIpAddresses>
+                        <Routes>
+                            <Route wcm:action="add">
+                                <Identifier>0</Identifier>
+                                <Prefix>{task_vars['routes_prefix']}</Prefix>
+                                <NextHopAddress>{task_vars['next_hop_address']}</NextHopAddress>
+                            </Route>
+                        </Routes>
+                    </Interface>
+                </Interfaces>
+            </component>
+            <component name="Microsoft-Windows-DNS-Client" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                <Interfaces>
+                    <Interface wcm:action="add">
+                        <Identifier>{task_vars['interface_identifier']}</Identifier>
+                        <DNSServerSearchOrder>
+                            <IpAddress wcm:action="add" wcm:keyValue="1">{task_vars['dns_server_address']}</IpAddress>
+                        </DNSServerSearchOrder>
+                    </Interface>
+                </Interfaces>
+            </component>"""
 
 
 class ActionModule(ActionBase):
@@ -31,13 +91,19 @@ class ActionModule(ActionBase):
     _VALID_ARGS = frozenset(
         (
             "dest",
+            "image_name",
             "admin_name",
             "admin_password",
             "user_name",
             "user_password",
             "install",
             "computer_name",
-            "first_logon_cmd"
+            "first_logon_cmd",
+            "network_interface",
+            "static_ip_address_cidr",
+            "static_route_cidr",
+            "static_gateway_ip",
+            "static_dns_server",
         )
     )
 
@@ -57,6 +123,9 @@ class ActionModule(ActionBase):
                 raise AnsibleAction(message="install argument is not provided")
 
             _task_vars = dict()
+            _task_vars["from_image_xml_code"] = image_name_xml_code(
+                self._task.args.get("image_name", None)
+            )
             _task_vars["admin_name"] = self._task.args.get("admin_name", None)
             _task_vars["admin_password"] = self._task.args.get("admin_password", None)
             _task_vars["user_name"] = self._task.args.get("user_name", None)
@@ -66,6 +135,9 @@ class ActionModule(ActionBase):
             _task_vars["first_logon_cmd"] = self._task.args.get(
                 "first_logon_cmd", default_first_logon_cmd
             )
+            
+            _task_vars['static_ip_xml_code'] = static_ip_xml_code(self)
+
             _task_vars["entry_point"] = default_entry_point
             _task_vars["main_code"] = default_main_code_file
             _task_vars["install_json"] = default_install_json_path
